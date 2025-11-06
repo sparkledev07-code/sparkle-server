4c1b5ff758d8de4d5d24396bd22839688b1fa5c0
sparkledev07 <sparkledev07@gmail.com>
2025-11-06 11:07:58 +0530

    check the server module

--- server.js
+++ server.js
@@ -14,5 +14,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", contactRoutes);
 
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running successfully on Render! Use /api/contact for API calls.");
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
